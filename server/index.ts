aad378875463d78f9a644adaf8145f1d4d783eef
Lee Nelson <lee@2fold.io>
2024-04-24 12:09:10 -0700

    updates server start point

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,12 @@
-// Server code here...
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
